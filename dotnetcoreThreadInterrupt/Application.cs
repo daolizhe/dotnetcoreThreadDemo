6cdece00b3977992ac9d06dc091c8290a0daa9d1
盗理者 <952651117@qq.com>
2018-11-17 10:44:07 +0800

    thread.IsBackground=true;
        thread.IsBackground=true;

--- dotnetcoreThreadInterrupt/Application.cs
+++ dotnetcoreThreadInterrupt/Application.cs
@@ -7,12 +7,12 @@ namespace dotnetcoreThreadInterrupt
         public void Start()
         {
             Thread thread = new Thread(TtoRun);
+            thread.IsBackground=true;
             thread.Start();
             for (int i = 0; i < 6; i++)
             {
                 Thread.Sleep(1000);
             }
-            thread.IsBackground=true;
             thread.Interrupt();
         }
         public void TtoRun()
